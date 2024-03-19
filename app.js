f263cf2c43f5cae8504a2fa1a0a6b9a734acd9d4
wanwisasin <138392704+wanwisasin@users.noreply.github.com>
2024-03-19 17:33:15 +0700

    product add

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./config/db');
+
+
+
+const {getProductPage,addProductPage , addProduct,deleteProduct,editProduct,editProductPage } = require('./backend/model/product');
+
+const app = express();
+const port = 2000;
+
+
+
+app.set('port', process.env.PORT || port);
+app.set('views', 'backend/views'); 
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(fileUpload()); 
+
+app.get('/', (req, res) => {
+    res.render('adminPage', { title: 'Admin Page' });
+});
+
+//product
+app.get('/productPage', getProductPage);
+app.get('/add/products',addProductPage);
+app.get('/edit/products/:id',editProductPage);
+app.get('/delete/products/:id',deleteProduct);
+app.post('/add/products',addProduct);
+app.post('/edit/products/:id',editProduct);
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
